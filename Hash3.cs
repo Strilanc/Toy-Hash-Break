576f379e906f93cb793141153d79c7e3a4656d0f
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:02:30 -0300

    Renamed a1 to a, b1 to b

--- Hash3.cs
+++ Hash3.cs
@@ -7,23 +7,23 @@ class Hash3 {
     public static HashState Hash(IEnumerable<Int32> data) {
         int inv3 = MathEx.MultiplicativeInverseS32(3).Value;
         unchecked {
-            Int32 a1 = 0;
+            Int32 a = 0;
             Int32 a2 = 0;
-            Int32 b1 = 0;
+            Int32 b = 0;
             Int32 b2 = 0;
             foreach (var e in data) {
                 for (var i = 0; i < 17; i++) {
-                    a1 *= -6;
-                    a1 += b1 + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e);
+                    a *= -6;
+                    a += b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e);
 
-                    b1 -= (b1 + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
-                    b1 *= inv3;
-                    b1 += a1 + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
+                    b -= (b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
+                    b *= inv3;
+                    b += a + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
                 }
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
                 a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e);
             }
-            return new HashState(a1 + a2, b1 + b2);
+            return new HashState(a + a2, b + b2);
         }
     }
 }
