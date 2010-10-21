4d61f13e44dd70f4e58518e773acce0842884c2b
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:24:01 -0300

    Inlined a3

--- Hash3.cs
+++ Hash3.cs
@@ -12,18 +12,15 @@ class Hash3 {
             Int32 b = 0;
             Int32 b2 = 0;
             foreach (var e in data) {
-                Int32 a3 = 0;
                 for (var i = 0; i < 17; i++) {
-                    a3 = (0x81BE - e) * MainHash.PowSumRevPowSum(inv3, -6, i);
-
                     a *= -6;
                     a += b;
 
                     b -= (b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
                     b *= inv3;
-                    b += a + a3 + b2 * MainHash.PowRevPowSum(inv3, -6, i + 1) + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
+                    b += a + (0x81BE - e) * MainHash.PowSumRevPowSum(inv3, -6, i) + b2 * MainHash.PowRevPowSum(inv3, -6, i + 1) + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
                 }
-                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e) + a3 + b2 * MainHash.PowRevPowSum(inv3, -6, 17);
+                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e) + (0x81BE - e) * MainHash.PowSumRevPowSum(inv3, -6, 16) + b2 * MainHash.PowRevPowSum(inv3, -6, 17);
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
             }
             return new HashState(a + a2, b + b2);
