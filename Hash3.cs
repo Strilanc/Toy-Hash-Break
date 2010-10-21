9b048c3e0b4aa2b1937c9e934748e6220c9edabc
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 19:14:09 -0300

    Extracted b10 modification from inner loop

--- Hash3.cs
+++ Hash3.cs
@@ -29,11 +29,12 @@ class Hash3 {
                               + 0x74FA * MainHash.PowSumRevPowSum(-6, inv3, i)
                               + b5 * inv3.Pow(i)
                               + -e * MainHash.PowSumRevPowSum(inv3, -6, i)
-                              + b9 * inv3.Pow(i) + a2 * -6 * MainHash.PowRevPowSum(inv3, -6, i);
+                              + b9 * inv3.Pow(i) + a2 * -6 * MainHash.PowRevPowSum(inv3, -6, i)
+                              + b10 * inv3.Pow(i) + (dn == 0 ? 0 : dn == 1 ? -879790284 : 710375220) * MainHash.PowRevPowSum(inv3, -6, i);
                     a *= -6;
-                    a += b + b7 + b8 + b10 + b11 + b12 + b13 + c + b4;
+                    a += b + b7 + b8 + b11 + b12 + b13 + c + b4;
 
-                    b -= (b + b7 + b8  + b10 + b11 + b12 + b13
+                    b -= (b + b7 + b8 + b11 + b12 + b13
                           + c 
                           + b4 
                           + inv3.Pow(17).PowSum(dn) * inv3.Pow(i) * -1268346242 
@@ -44,7 +45,6 @@ class Hash3 {
                     b4 *= inv3;
                     b7 *= inv3;
                     b8 *= inv3;
-                    b10 *= inv3;
                     b11 *= inv3;
                     b12 *= inv3;
                     b13 *= inv3;
@@ -52,11 +52,11 @@ class Hash3 {
                     b += a;
                     b7 += inv3.Pow(17).PowSum(dn) * -1268346242 * MainHash.PowRevPowSum(inv3, -6, i + 1);
                     b8 += b6 * MainHash.PowRevPowSum(inv3, -6, i + 1);
-                    b10 += (dn == 0 ? 0 : dn == 1 ? -2000851934 : 2029087778) * (-6).Pow(i + 1);
                     b11 += a4 * (-6).Pow(i + 1);
                     b12 += a5 * (-6).Pow(i + 1);
                     b13 += -e * MainHash.PowSumRevPowSum(inv3, -6, i);
                 }
+                b10 = b10 * inv3.Pow(17) + (dn == 0 ? 0 : dn == 1 ? -879790284 : 710375220) * MainHash.PowRevPowSum(inv3, -6, 17);
                 b9 = b9 * inv3.Pow(17) + a2 * -6 * MainHash.PowRevPowSum(inv3, -6, 17);
                 a2 = a2 * (-6).Pow(17) - e * 1811343553;
                 a4 = a4 * (-6).Pow(17) + inv3.Pow(17).PowSum(dn) * -1542355254;
