db864ea8f9f044f25fd8fe5be9441c5c4b99661a
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 12:51:20 -0300

    Split a and b into linear and non-linear parts

--- Hash3.cs
+++ Hash3.cs
@@ -7,22 +7,26 @@ class Hash3 {
     public static HashState Hash(IEnumerable<Int32> data) {
         int inv3 = MathEx.MultiplicativeInverseS32(3).Value;
         unchecked {
-            Int32 a = 0;
-            Int32 b = 0;
+            Int32 a1 = 0;
+            Int32 a2 = 0;
+            Int32 b1 = 0;
+            Int32 b2 = 0;
             foreach (var e in data) {
                 for (var i = 0; i < 17; i++) {
-                    a *= -6;
-                    a += b;
-                    a += 0x74FA;
-                    a -= e;
-                    b -= b % 3;
-                    b *= inv3;
-                    b += a;
-                    b += 0x81BE;
-                    b -= e;
+                    a1 *= -6;
+                    a2 *= -6;
+                    a1 += b1 + b2;
+                    a2 += 0x74FA;
+                    a2 -= e;
+                    b1 -= (b1 + b2) % 3;
+                    b1 *= inv3;
+                    b2 *= inv3;
+                    b1 += a1 + a2;
+                    b2 += 0x81BE;
+                    b2 -= e;
                 }
             }
-            return new HashState(a, b);
+            return new HashState(a1 + a2, b1 + b2);
         }
     }
 }
