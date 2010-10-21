2943264acaa07a3296ec1193d90161918d739709
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:07:40 -0300

    Scoped a3 inside outer loop

--- Hash3.cs
+++ Hash3.cs
@@ -9,24 +9,25 @@ class Hash3 {
         unchecked {
             Int32 a = 0;
             Int32 a2 = 0;
-            Int32 a3 = 0;
             Int32 b = 0;
             Int32 b2 = 0;
             foreach (var e in data) {
+                Int32 a3 = 0;
                 for (var i = 0; i < 17; i++) {
-                    a *= -6;
-                    a += b;
                     a3 *= -6;
                     a3 += b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e);
+
+                    a *= -6;
+                    a += b;
 
                     b -= (b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
                     b *= inv3;
                     b += a + a3 + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
                 }
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
-                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e);
+                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e) + a3;
             }
-            return new HashState(a + a2 + a3, b + b2);
+            return new HashState(a + a2, b + b2);
         }
     }
 }
