fa253600f4c9b3418b2292c1e41158963a89c1e2
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:28:55 -0300

    Reduced some constant expressions to constants

--- Hash3.cs
+++ Hash3.cs
@@ -22,7 +22,7 @@ class Hash3 {
                     b += a + (0x81BE - e) * MainHash.PowSumRevPowSum(inv3, -6, i) + b2 * MainHash.PowRevPowSum(inv3, -6, i + 1) + (a2+a3) * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
                 }
                 a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * -e + -e * 1783724810 + b2 * 270124635;
-                a3 = a3 * (-6).Pow(17) + (-6).PowSum(17) * 0x74FA + 0x81BE * 1783724810;
+                a3 = a3 * (-6).Pow(17) - 2000851934;
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
             }
             return new HashState(a + a2 + a3, b + b2);
