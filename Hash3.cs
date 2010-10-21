a693772b82800206352204161971e2c1f27ca696
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 12:59:52 -0300

    Extracted a2 modifications out of inner loop

--- Hash3.cs
+++ Hash3.cs
@@ -12,22 +12,19 @@ class Hash3 {
             Int32 b1 = 0;
             Int32 b2 = 0;
             foreach (var e in data) {
-                Int32 pa2 = a2;
                 for (var i = 0; i < 17; i++) {
                     a1 *= -6;
                     a1 += b1 + b2;
 
-                    a2 = pa2 * (-6).Pow(i) + (-6).PowSum(i) * (0x74FA - e);
-
                     b1 -= (b1 + b2) % 3;
                     b1 *= inv3;
-                    b1 += a1 + a2;
+                    b1 += a1 + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
 
                     b2 *= inv3;
                     b2 += 0x81BE;
                     b2 -= e;
                 }
-                a2 = pa2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e);
+                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e);
             }
             return new HashState(a1 + a2, b1 + b2);
         }
