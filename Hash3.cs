6dbaa2d37fddc75fdcd8ff16d17941dcede93b4d
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:40:31 -0300

    Extracted b3 and b4 from b

--- Hash3.cs
+++ Hash3.cs
@@ -12,17 +12,21 @@ class Hash3 {
             Int32 a3 = 0;
             Int32 b = 0;
             Int32 b2 = 0;
+            Int32 b3 = 0;
+            Int32 b4 = 0;
             Int32 dn = 0;
             foreach (var e in data) {
                 for (var i = 0; i < 17; i++) {
                     a *= -6;
-                    a += b;
+                    a += b + b3 + b4;
 
-                    b -= (b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
+                    b -= (b + b3 + b4 + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
                     b *= inv3;
+                    b3 *= inv3;
+                    b4 *= inv3;
+                    b3 += 0x74FA * (-6).PowSum(i + 1);
+                    b4 += 0x81BE * MainHash.PowSumRevPowSum(inv3, -6, i);
                     b += a
-                         + 0x74FA * (-6).PowSum(i + 1)
-                         + 0x81BE * MainHash.PowSumRevPowSum(inv3, -6, i) 
                          + b2 * MainHash.PowRevPowSum(inv3, -6, i + 1) 
                          + a2 * (-6).Pow(i + 1) 
                          + a3 * (-6).Pow(i + 1) 
@@ -34,7 +38,7 @@ class Hash3 {
                 a3 = dn == 1 ? -2000851934 : 2029087778;
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
             }
-            return new HashState(a + a2 + a3, b + b2);
+            return new HashState(a + a2 + a3, b + b2 + b3 + b4);
         }
     }
 }
