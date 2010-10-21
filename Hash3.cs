e50c166f0af873e1697000efbe5dc4a93be8473e
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 18:44:00 -0300

    Split b7 through b13 from b

--- Hash3.cs
+++ Hash3.cs
@@ -15,6 +15,13 @@ class Hash3 {
             Int32 b4 = 0;
             Int32 b5 = 0;
             Int32 b6 = 0;
+            Int32 b7 = 0;
+            Int32 b8 = 0;
+            Int32 b9 = 0;
+            Int32 b10 = 0;
+            Int32 b11 = 0;
+            Int32 b12 = 0;
+            Int32 b13 = 0;
             Int32 dn = 0;
             foreach (var e in data) {
                 for (var i = 0; i < 17; i++) {
@@ -23,9 +30,9 @@ class Hash3 {
                               + b5 * inv3.Pow(i)
                               + -e * MainHash.PowSumRevPowSum(inv3, -6, i);
                     a *= -6;
-                    a += b + c + b4;
+                    a += b + b7 + b8 + b9 + b10 + b11 + b12 + b13 + c + b4;
 
-                    b -= (b 
+                    b -= (b + b7 + b8 + b9 + b10 + b11 + b12 + b13
                           + c 
                           + b4 
                           + inv3.Pow(17).PowSum(dn) * inv3.Pow(i) * -1268346242 
@@ -34,15 +41,22 @@ class Hash3 {
                           - inv3.PowSum(i) * e) % 3;
                     b *= inv3;
                     b4 *= inv3;
+                    b7 *= inv3;
+                    b8 *= inv3;
+                    b9 *= inv3;
+                    b10 *= inv3;
+                    b11 *= inv3;
+                    b12 *= inv3;
+                    b13 *= inv3;
                     b4 += 0x81BE * MainHash.PowSumRevPowSum(inv3, -6, i);
-                    b += a
-                         + inv3.Pow(17).PowSum(dn) * -1268346242 * MainHash.PowRevPowSum(inv3, -6, i + 1) 
-                         + b6 * MainHash.PowRevPowSum(inv3, -6, i + 1)
-                         + a2 * (-6).Pow(i + 1)
-                         + (dn == 0 ? 0 : dn == 1 ? -2000851934 : 2029087778) * (-6).Pow(i + 1)
-                         + a4 * (-6).Pow(i + 1)
-                         + a5 * (-6).Pow(i + 1)
-                         - e * MainHash.PowSumRevPowSum(inv3, -6, i);
+                    b += a;
+                    b7 += inv3.Pow(17).PowSum(dn) * -1268346242 * MainHash.PowRevPowSum(inv3, -6, i + 1);
+                    b8 += b6 * MainHash.PowRevPowSum(inv3, -6, i + 1);
+                    b9 += a2 * (-6).Pow(i + 1);
+                    b10 += (dn == 0 ? 0 : dn == 1 ? -2000851934 : 2029087778) * (-6).Pow(i + 1);
+                    b11 += a4 * (-6).Pow(i + 1);
+                    b12 += a5 * (-6).Pow(i + 1);
+                    b13 += -e * MainHash.PowSumRevPowSum(inv3, -6, i);
                 }
                 a2 = a2 * (-6).Pow(17) - e * 1811343553;
                 a4 = a4 * (-6).Pow(17) + inv3.Pow(17).PowSum(dn) * -1542355254;
@@ -58,7 +72,7 @@ class Hash3 {
                 + a4 + a5, 
                 b 
                 + inv3.Pow(17).PowSum(dn) * -624544992 
-                + b4 + b5 + b6);
+                + b4 + b5 + b6 + b7 + b8 + b9 + b10 + b11 + b12 + b13);
         }
     }
 }
