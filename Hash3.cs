9036eb925985cf9e979447f796069e11c211ffb2
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 13:10:09 -0300

    Extracted a4 from a3

--- Hash3.cs
+++ Hash3.cs
@@ -13,19 +13,22 @@ class Hash3 {
             Int32 b2 = 0;
             foreach (var e in data) {
                 Int32 a3 = 0;
+                Int32 a4 = 0;
                 for (var i = 0; i < 17; i++) {
                     a3 *= -6;
-                    a3 += b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e);
+                    a4 *= -6;
+                    a3 += (inv3).PowSum(i) * (0x81BE - e);
+                    a4 += b2 * (inv3).Pow(i);
 
                     a *= -6;
                     a += b;
 
                     b -= (b + b2 * (inv3).Pow(i) + (inv3).PowSum(i) * (0x81BE - e)) % 3;
                     b *= inv3;
-                    b += a + a3 + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
+                    b += a + a3 + a4 + a2 * (-6).Pow(i + 1) + (-6).PowSum(i + 1) * (0x74FA - e);
                 }
                 b2 = b2 * (inv3).Pow(17) + (inv3).PowSum(17) * (0x81BE - e);
-                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e) + a3;
+                a2 = a2 * (-6).Pow(17) + (-6).PowSum(17) * (0x74FA - e) + a3 + a4;
             }
             return new HashState(a + a2, b + b2);
         }
