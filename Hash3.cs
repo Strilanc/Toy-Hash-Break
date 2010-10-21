e7dfacfb986172eaf0fea0a2c6611700233042ca
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 16:13:03 -0300

    Folded some expressions

--- Hash3.cs
+++ Hash3.cs
@@ -35,12 +35,12 @@ class Hash3 {
                          - e * MainHash.PowSumRevPowSum(inv3, -6, i);
                 }
                 b5 = b5 * inv3.Pow(17) - e * 2053849445;
-                a2 = a2 * (-6).Pow(17) - e * 1811343553 + (inv3.Pow(17).PowSum(dn) * -1268346242 + b6) * 270124635;
+                a2 = a2 * (-6).Pow(17) - e * 1811343553 + inv3.Pow(17).PowSum(dn) * -1542355254 + b6 * 270124635;
                 dn += 1;
                 a3 = dn == 1 ? -2000851934 : 2029087778;
                 b6 = b6 * inv3.Pow(17) - inv3.PowSum(17) * e;
             }
-            return new HashState(a + a2 + a3, b + inv3.Pow(17).PowSum(dn) * -1268346242 + b6 + inv3.Pow(17).PowSum(dn) * 643801250 + b4 + b5);
+            return new HashState(a + a2 + a3, b + b6 + inv3.Pow(17).PowSum(dn) * -624544992 + b4 + b5);
         }
     }
 }
