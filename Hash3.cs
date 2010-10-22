f790aeaa0b04de756112578cc55d529d05972bc2
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 22:56:47 -0300

    Extracted b4 assignment from inner loop

--- Hash3.cs
+++ Hash3.cs
@@ -21,7 +21,6 @@ class Hash3 {
             Int32 dn = 0;
             var ee = new List<Int32>();
             foreach (var e in data) {
-                Int32 pb4 = b4;
                 for (var i = 0; i < 17; i++) {
                     Int32 c = 643801250 * i3p17.PowSum(dn) * i3.Pow(i)
                               + 0x74FA * MathEx.TrianglePowSum(-6, i3, i)
@@ -33,19 +32,17 @@ class Hash3 {
                               + b7
                               + b8
                               + ee.RevDot(i3p17.Powers()) * 454799799 * i3.Pow(i)
-                              + -e * i3.Powers().Take(i - 1).RevDot(MathEx.TrianglePowSums(i3, -6));
+                              + -e * i3.Powers().Take(i - 1).RevDot(MathEx.TrianglePowSums(i3, -6))
+                              + b4 * i3.Pow(i) + 0x81BE * i3.Powers().Take(i - 1).RevDot(MathEx.TrianglePowSums(i3, -6));
                     a *= -6;
-                    a += b + c + b4;
+                    a += b + c;
 
                     b -= (b
                           + c 
-                          + b4 
                           + i3p17.PowSum(dn) * i3.Pow(i) * -1268346242
                           + ee.RevDot(i3p17.Powers()) * -i3.PowSum(17) * i3.Pow(i) 
                           + i3.PowSum(i) * 0x81BE 
                           - i3.PowSum(i) * e) % 3;
-                    
-                    b4 = pb4 * i3.Pow(i + 1) + 0x81BE * i3.Powers().Take(i).RevDot(MathEx.TrianglePowSums(i3, -6));
                     
                     b7 *= i3;
                     b7 += i3p17.PowSum(dn) * -1268346242 * MathEx.DiagonalPowSum(i3, -6, i + 1);
@@ -57,6 +54,7 @@ class Hash3 {
                     b += a;
                 }
 
+                b4 = b4 * i3.Pow(17) + 0x81BE * i3.Powers().Take(16).RevDot(MathEx.TrianglePowSums(i3, -6));
                 b9 = b9 * i3p17 + ee.RevDot(m6p17.Powers()) * -951417952;
                 b10 = dn == 0 ? 0 : 910530428 * i3p17.Pow(dn - 1) + -1755474052 * i3p17.PowSum(dn - 1);
                 b11 = b11 * i3p17 + MathEx.TrianglePowSum(m6p17, i3p17, Math.Max(dn - 1, 0)) * 46210348;
