479dc3f5cff8eacc5f81780af0de710868b1cbfa
Strilanc <Craig.Gidney@gmail.com>
2010-10-21 22:42:53 -0300

    Named some constants

--- Hash3.cs
+++ Hash3.cs
@@ -5,7 +5,9 @@ using System.Text;
 
 class Hash3 {
     public static HashState Hash(IEnumerable<Int32> data) {
-        int inv3 = MathEx.MultiplicativeInverseS32(3).Value;
+        int i3 = MathEx.MultiplicativeInverseS32(3).Value;
+        int i3p17 = i3.Pow(17);
+        int m6p17 = (-6).Pow(17);
         unchecked {
             Int32 a = 0;
             Int32 b = 0;
@@ -21,14 +23,14 @@ class Hash3 {
             var ee = new List<Int32>();
             foreach (var e in data) {
                 for (var i = 0; i < 17; i++) {
-                    Int32 c = 643801250 * inv3.Pow(17).PowSum(dn) * inv3.Pow(i)
-                              + 0x74FA * MathEx.TrianglePowSum(-6, inv3, i)
-                              + ee.RevDot(inv3.Pow(17).Powers()) * -2053849445 * inv3.Pow(i)
-                              + -e * MathEx.TrianglePowSum(inv3, -6, i)
-                              + b9 * inv3.Pow(i) + ee.RevDot((-6).Pow(17).Powers()) * -1995367200 * MathEx.DiagonalPowSum(inv3, -6, i)
-                              + b10 * inv3.Pow(i) + (dn == 0 ? 0 : dn == 1 ? -879790284 : 710375220) * MathEx.DiagonalPowSum(inv3, -6, i)
-                              + b11 * inv3.Pow(i) + 664196932 * MathEx.TrianglePowSum((-6).Pow(17), inv3.Pow(17), Math.Max(dn - 1, 0)) * MathEx.DiagonalPowSum(inv3, -6, i)
-                              + b12 * inv3.Pow(i) + ee.RevDot(MathEx.DiagonalPowSums((-6).Pow(17), inv3.Pow(17))) * -21473370 * MathEx.DiagonalPowSum(inv3, -6, i)
+                    Int32 c = 643801250 * i3p17.PowSum(dn) * i3.Pow(i)
+                              + 0x74FA * MathEx.TrianglePowSum(-6, i3, i)
+                              + ee.RevDot(i3p17.Powers()) * -2053849445 * i3.Pow(i)
+                              + -e * MathEx.TrianglePowSum(i3, -6, i)
+                              + b9 * i3.Pow(i) + ee.RevDot(m6p17.Powers()) * -1995367200 * MathEx.DiagonalPowSum(i3, -6, i)
+                              + b10 * i3.Pow(i) + (dn == 0 ? 0 : dn == 1 ? -879790284 : 710375220) * MathEx.DiagonalPowSum(i3, -6, i)
+                              + b11 * i3.Pow(i) + 664196932 * MathEx.TrianglePowSum(m6p17, i3p17, Math.Max(dn - 1, 0)) * MathEx.DiagonalPowSum(i3, -6, i)
+                              + b12 * i3.Pow(i) + ee.RevDot(MathEx.DiagonalPowSums(m6p17, i3p17)) * -21473370 * MathEx.DiagonalPowSum(i3, -6, i)
                               + b7
                               + b8
                               + b13;
@@ -38,46 +40,46 @@ class Hash3 {
                     b -= (b
                           + c 
                           + b4 
-                          + inv3.Pow(17).PowSum(dn) * inv3.Pow(i) * -1268346242
-                          + ee.RevDot(inv3.Pow(17).Powers()) * -inv3.PowSum(17) * inv3.Pow(i) 
-                          + inv3.PowSum(i) * 0x81BE 
-                          - inv3.PowSum(i) * e) % 3;
+                          + i3p17.PowSum(dn) * i3.Pow(i) * -1268346242
+                          + ee.RevDot(i3p17.Powers()) * -i3.PowSum(17) * i3.Pow(i) 
+                          + i3.PowSum(i) * 0x81BE 
+                          - i3.PowSum(i) * e) % 3;
                     
-                    b4 *= inv3;
-                    b4 += 0x81BE * MathEx.TrianglePowSum(inv3, -6, i);
+                    b4 *= i3;
+                    b4 += 0x81BE * MathEx.TrianglePowSum(i3, -6, i);
                     
-                    b7 *= inv3;
-                    b7 += inv3.Pow(17).PowSum(dn) * -1268346242 * MathEx.DiagonalPowSum(inv3, -6, i + 1);
+                    b7 *= i3;
+                    b7 += i3p17.PowSum(dn) * -1268346242 * MathEx.DiagonalPowSum(i3, -6, i + 1);
                     
-                    b8 *= inv3;
-                    b8 += ee.RevDot(inv3.Pow(17).Powers()) * -inv3.PowSum(17) * MathEx.DiagonalPowSum(inv3, -6, i + 1);
+                    b8 *= i3;
+                    b8 += ee.RevDot(i3p17.Powers()) * -i3.PowSum(17) * MathEx.DiagonalPowSum(i3, -6, i + 1);
                     
-                    b13 *= inv3;
-                    b13 += -e * MathEx.TrianglePowSum(inv3, -6, i);
+                    b13 *= i3;
+                    b13 += -e * MathEx.TrianglePowSum(i3, -6, i);
 
-                    b *= inv3;
+                    b *= i3;
                     b += a;
                 }
 
-                b9 = b9 * inv3.Pow(17) + ee.RevDot((-6).Pow(17).Powers()) * -951417952;
-                b10 = dn == 0 ? 0 : 910530428 * inv3.Pow(17).Pow(dn - 1) + -1755474052 * inv3.Pow(17).PowSum(dn - 1);
-                b11 = b11 * inv3.Pow(17) + MathEx.TrianglePowSum((-6).Pow(17), inv3.Pow(17), Math.Max(dn - 1, 0)) * 46210348;
-                b12 = b12 * inv3.Pow(17) + ee.RevDot(MathEx.DiagonalPowSums((-6).Pow(17), inv3.Pow(17))) * 243764226;
+                b9 = b9 * i3p17 + ee.RevDot(m6p17.Powers()) * -951417952;
+                b10 = dn == 0 ? 0 : 910530428 * i3p17.Pow(dn - 1) + -1755474052 * i3p17.PowSum(dn - 1);
+                b11 = b11 * i3p17 + MathEx.TrianglePowSum(m6p17, i3p17, Math.Max(dn - 1, 0)) * 46210348;
+                b12 = b12 * i3p17 + ee.RevDot(MathEx.DiagonalPowSums(m6p17, i3p17)) * 243764226;
 
                 ee.Add(e);
                 dn += 1;
             }
             return new HashState(
                 a
-                + ee.RevDot((-6).Pow(17).Powers()) * -1814922448
+                + ee.RevDot(m6p17.Powers()) * -1814922448
                 + (dn == 0 ? 0 : dn == 1 ? -2000851934 : 2029087778)
-                + -1542355254 * MathEx.TrianglePowSum((-6).Pow(17), inv3.Pow(17), dn - 1)
-                + ee.RevDot(MathEx.DiagonalPowSums((-6).Pow(17), inv3.Pow(17))) * 267738705 * -inv3.PowSum(17), 
+                + -1542355254 * MathEx.TrianglePowSum(m6p17, i3p17, dn - 1)
+                + ee.RevDot(MathEx.DiagonalPowSums(m6p17, i3p17)) * 267738705 * -i3.PowSum(17), 
                 b 
-                + inv3.Pow(17).PowSum(dn) * -624544992 
+                + i3p17.PowSum(dn) * -624544992 
                 + b4
-                + ee.RevDot(inv3.Pow(17).Powers()) * -2053849445
-                + ee.RevDot(inv3.Pow(17).Powers()) * -inv3.PowSum(17)
+                + ee.RevDot(i3p17.Powers()) * -2053849445
+                + ee.RevDot(i3p17.Powers()) * -i3.PowSum(17)
                 + b7 + b8 + b9 + b10 + b11 + b12 + b13);
         }
     }
